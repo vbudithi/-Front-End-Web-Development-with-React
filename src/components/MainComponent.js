ad48473b7962722f04b785309242e6c6e30a9284
vbudithi <vivek.budithi@gmail.com>
2020-09-29 22:42:12 +0930

    Restructure Menu and Dishdetail as a functional component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -30,7 +30,7 @@ class Main extends Component {
          <Menu dishes ={this.state.dishes} 
                onClick ={(dishId) => this.onDishSelect(dishId)}/>
          <DishDetail 
-              dish = {this.state.dishes.filter((dish) => dish.id === this.state.selectedDish ) [0]} />
+              dish = {this.state.dishes.filter((dish) => dish.id === this.state.selectedDish )[0]} />
       </div>
     );
   }
